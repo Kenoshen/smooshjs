13df78546fd4e0fd5a6135f17b28e3be04b1ebab
Michael Wingfield <michael.wingfield@twcable.com>
2015-11-02 09:43:16 -0700

    added tests for interpreting the command line arguments

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+#! /usr/bin/env node
+
 var fs = require("fs");
+var path = require("path");
 
 function displayMessage(name){ console.log(fs.readFileSync(__dirname + "/messages/" + name + ".txt", "utf8")); }
 
 var helpTag = false;
 var amdTag = false;
 var cjsTag = false;
+var testTag = false;
 var entryPoint = undefined;
 var output = undefined;
 
 for (var i = 2; i < process.argv.length; i++){
     var arg = process.argv[i];
-    if (arg === "--help" || arg === "-help" || arg === "help"){
+    if (arg === "--help" || arg === "-help"){
         helpTag = true;
-    } else if (arg === "--amd" || arg === "-amd" || arg === "amd"){
+    } else if (arg === "--amd" || arg === "-amd"){
         amdTag = true;
-    } else if (arg === "--cjs" || arg === "-cjs" || arg === "cjs"){
+    } else if (arg === "--cjs" || arg === "-cjs"){
         cjsTag = true;
+    } else if (arg === "--test" || arg === "-test"){
+        testTag = true;
     } else if (! entryPoint){
         entryPoint = arg;
     } else {
@@ -35,19 +41,56 @@ if (entryPoint.indexOf(".js") < 0){
     console.log("entry point must be a javascript file (end in .js)");
     process.exit(1);
 }
-if (!output){
-    output = entryPoint.split(".js")[0] + ".smooshed.js";
-    console.log("Output file will be set to: " + output);
-}
-if (!amd && !cjs){
+
+if (!amdTag && !cjsTag){
     console.log("must provide at least one of the output tags (-amd  OR  -cjs)");
     process.exit(1);
 }
-if (amd && cjs){
+if (amdTag && cjsTag){
     // TODO: figure out what to do when they want both
 }
 
 // TODO: check if entryPoint file exists
+var entryPointStat;
+try {
+    entryPointStat = fs.statSync(entryPoint);
+} catch (e){
+    console.log("file at " + entryPoint + " does not exist");
+    process.exit(1);
+}
+var entryPointPathParsed = path.parse(entryPoint);
+
+if (entryPointStat.isDirectory()){
+    console.log("file at " + entryPoint + " is a directory");
+    process.exit(1);
+}
+
+if (!output){
+    output = entryPointPathParsed.dir + "/" + entryPointPathParsed.name + ".smooshed.js";
+    console.log("output file will be set to: " + output);
+}
+
+var outputStat;
+try {
+    outputStat = fs.statSync(output);
+
+    if (outputStat.isFile()){
+        console.log("the output file at " + output + " already exists, will try to overwrite it");
+    }
+
+    if (outputStat.isDirectory()){
+        var outputDir = output + (output.substr(output.length - 1) === "/" ? "" : "/");
+        output = outputDir + entryPointPathParsed.base;
+        console.log("the output file at " + outputDir + " is a directory, output will be set to " + output);
+    }
+} catch (e){
+    // do nothing because the file doesn't exist yet
+}
+
+if (testTag){
+    console.log("test tag was found, exiting now");
+    process.exit(0);
+}
 // TODO: write dependency tree builder
 // TODO: write wrapper functions for amd and commonjs
 // TODO: figure out how to handle local vs 3rd party dependencies
